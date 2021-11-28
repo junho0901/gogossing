4b63ade7f4128f03f2c8c6db0560afadd881ca3c
lss980301 <62423529+lss980301@users.noreply.github.com>
2021-11-28 22:59:33 +0900

    Update mail.py

--- src/mail.py
+++ src/mail.py
@@ -1,15 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 import smtplib
-from email.mime.text import MINEText
-#GPIO.setmode(GPIO.BCM)
-#CHECK_ON=1
-#GPIO.setup(
-smtp=smtplib.SMTP('smtp.gmail.com',587)
+from email.mime.text import MIMEText
+
+
+smtp = smtplib.SMTP('smtp.gmail.com',587)
 smtp.starttls()
-smtp.login('seongsabgim@gmail.com', 'taeyeon31$2!')
-msg=MINEText('mumu is jonjal')
-msg['Subject']='mumu is sexy'
-msg['To']='lss980301@naver.com'
+smtp.login('seongsabgim@gmail.com', 'ggmzyrdpzjlkqpow')
+msg=MIMEText('무성이가 위험합니다')
+msg['Subject'] = '무성이를 도와주세요!'
+msg['To'] = 'lss980301@naver.com'
 smtp.sendmail('seongsabgim@gmail.com', 'lss980301@naver.com', msg.as_string())
 smtp.quit()
