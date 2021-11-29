d2392126ab7f2472ad68696f2cd6cbcaf1be2fb3
junho0901 <62511499+junho0901@users.noreply.github.com>
2021-11-29 21:15:59 +0900

    Update AlarmChecker.py

--- src/mainboard/AlarmChecker.py
+++ src/mainboard/AlarmChecker.py
@@ -4,23 +4,22 @@
 import RPi.GPIO as GPIO
 import time
 
-A_BUZ_PIN = 21
+GPIO.setwarnings(False)
+BUZ = 12
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(A_BUZ_PIN,GPIO.OUT)
-GPIO.output(A_BUZ_PIN,GPIO.LOW)
-    
-def getsound():
-    try:
-        GPIO.output(A_BUZ_PIN, GPIO.HIGH)
-        print("BUZZER ON")
-        time.sleep(2)
-            
-        GPIO.output(A_BUZ_PIN,GPIO.LOW)
+def setPinSound():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUZ,GPIO.OUT)
+    GPIO.output(BUZ,GPIO.LOW)
+
+def set_sound(check):
+    if check ==ON:
+         GPIO.output(BUZ, GPIO.HIGH)
+         print("BUZZER ON")
+         time.sleep(2)
+    else:
+        GPIO.output(BUZ,GPIO.LOW)
         print("BUZZER OFF")
         time.sleep(2)
-            
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-    return 0
+        
