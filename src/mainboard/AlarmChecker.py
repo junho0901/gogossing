5061661947cdf83e062ff39db5f2afa6e02d1c7b
junho0901 <62511499+junho0901@users.noreply.github.com>
2021-11-29 15:08:34 +0900

    Create AlarmChecker.py

---
+++ src/mainboard/AlarmChecker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+import time
+
+A_BUZ_PIN = 21
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(A_BUZ_PIN,GPIO.OUT)
+GPIO.output(A_BUZ_PIN,GPIO.LOW)
+    
+def getsound():
+    try:
+        GPIO.output(A_BUZ_PIN, GPIO.HIGH)
+        print("BUZZER ON")
+        time.sleep(2)
+            
+        GPIO.output(A_BUZ_PIN,GPIO.LOW)
+        print("BUZZER OFF")
+        time.sleep(2)
+            
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+    return 0
