bbf12f5a6732e693b5ade5995363c3223dcf5eab
서무성 <62416298+seomuseong1614@users.noreply.github.com>
2021-11-29 20:43:38 +0900

    Update MailController.py

--- src/mainboard/MailController.py
+++ src/mainboard/MailController.py
@@ -1,16 +1,21 @@
-import RPi.GPIO as GPIO
-# devide "import time,smtplib" into two sentences shown line3, and line4
-import time       
-import smtplib 
+# devide "import time,smtplib" into two sentences shown line4, and line5
+import json
+import requests
+import smtplib
 from email.mime.text import MIMEText
 
-def send_mail():    # sendmail() --> send_mail()
+
+def send_mail():  # sendmail() --> send_mail()
+    url = 'http://ip-api.com/json'
+    data = requests.get(url)
+    res = data.json()
+    text = "위도: " + str(res.get('lat')) + "\n" +  "경도: " + str(res.get('lon'))
+
     smtp = smtplib.SMTP('smtp.gmail.com',587)
     smtp.starttls()
     smtp.login('seongsabgim@gmail.com', 'ggmzyrdpzjlkqpow')
-    msg=MIMEText('무성이가 위험합니다')
-    msg['Subject'] = '무성이를 도와주세요!'
+    msg=MIMEText(text)
+    msg['Subject'] = '[고고씽 with safety] 킥보드 사용자가 위험합니다.'
     msg['To'] = 'lss980301@naver.com'
     smtp.sendmail('seongsabgim@gmail.com', 'lss980301@naver.com', msg.as_string())
     smtp.quit()
- 
