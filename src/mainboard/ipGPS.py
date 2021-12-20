443d5bffceb4fe4f08ad272f03679ce3d5d50a4a
서무성 <62416298+seomuseong1614@users.noreply.github.com>
2021-12-20 19:46:52 +0900

    Create ipGPS.py

---
+++ src/mainboard/ipGPS.py
@@ -0,0 +1,21 @@
+# devide "import time,smtplib" into two sentences shown line4, and line5
+import json
+import requests
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail():  # sendmail() --> send_mail()
+    url = 'http://ip-api.com/json'
+    data = requests.get(url)
+    res = data.json()
+    text = "위도: " + str(res.get('lat')) + "\n" +  "경도: " + str(res.get('lon'))
+    #35.1537926, 128.0990911
+    smtp = smtplib.SMTP('smtp.gmail.com',587)
+    smtp.starttls()
+    smtp.login('seongsabgim@gmail.com', 'ggmzyrdpzjlkqpow')
+    msg=MIMEText(text)
+    msg['Subject'] = '[고고씽 with safety] 킥보드 사용자가 위험합니다.'
+    msg['To'] = 'lss980301@naver.com'
+    smtp.sendmail('seongsabgim@gmail.com', 'lss980301@naver.com', msg.as_string())
+    smtp.quit()
